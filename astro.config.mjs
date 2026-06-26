93635894b55b0b9a5ba2045c16bf61953ae0b8ad
James Basoo <jamesbasoo@gmail.com>
2026-06-26 16:39:01 +0200

    Upgrade and add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,42 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jamesbasoo.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.fontsource(),
-			name: 'Atkinson Hyperlegible Next',
-			cssVariable: '--font-atkinson',
-			weights: ["100 900"],
-			styles: ["normal", "italic"]
-		},
-		{
-			provider: fontProviders.fontsource(),
-			name: 'Atkinson Hyperlegible Mono',
-			cssVariable: '--font-atkinson-mono',
-			weights: ["100 900"],
-			styles: ["normal", "italic"]
-		},
-	],
+    site: 'https://jamesbasoo.com',
+    integrations: [mdx(), sitemap()],
+
+    fonts: [
+        {
+            provider: fontProviders.fontsource(),
+            name: 'Atkinson Hyperlegible Next',
+            cssVariable: '--font-atkinson',
+            weights: ["100 900"],
+            styles: ["normal", "italic"]
+        },
+        {
+            provider: fontProviders.fontsource(),
+            name: 'Atkinson Hyperlegible Mono',
+            cssVariable: '--font-atkinson-mono',
+            weights: ["100 900"],
+            styles: ["normal", "italic"]
+        },
+    ],
+
+    adapter: netlify(),
+
+    vite: {
+        resolve: {
+            alias: {
+                '@src': `${__dirname}/src`,
+            },
+        },
+    },
 });
